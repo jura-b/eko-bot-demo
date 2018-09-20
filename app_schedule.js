bc7e0e9f3863579ccb3ba0da3d1bbb7cec797f2a
Jura Boonnom <jura@ekoapp.com>
2018-09-20 12:51:32 +0700

    - configurable schedule

--- app_schedule.js
+++ app_schedule.js
@@ -4,9 +4,16 @@ require('dotenv').config();
 const ScheduleReportService = require('./services/ScheduleReportService');
 
 const scheduleReportService = new ScheduleReportService();
-scheduleReportService.setSchedule({
-  minute: 0,
-});
+const schedule = {};
+
+if (process.env.SCHEDULE_HOUR) {
+  schedule.hour = Number(process.env.SCHEDULE_HOUR);
+}
+if (process.env.SCHEDULE_MINUTE) {
+  schedule.minute = Number(process.env.SCHEDULE_MINUTE);
+}
+
+scheduleReportService.setSchedule(schedule);
 
 scheduleReportService.start(require('./jobs/notify-summary-report'));
 require('./jobs/notify-summary-report')();
